0064d479010e33c0ae6a31ab2e957367500c5306
Soenyoeknoe <luaeysoen99@gmail.com>
2024-07-24 22:35:47 +1000

    remove bug

--- app/committee/page.tsx
+++ app/committee/page.tsx
@@ -1,28 +1,13 @@
 'use client'
 import React from 'react';
 
-import { useEffect } from 'react'
-import Cursor from '@/components/committeeCursor'
-import Gallery from '@/components/committeeGallery'
-import Navigation from '@/components/committeeNavigation'
-import Footer from '@/components/Footer'
+import { useEffect } from 'react';
 
 const committee = () => {
-  useEffect(() => {
-    const handleScroll = () => {
-      // Implement scroll logic here if needed
-    }
-
-    window.addEventListener('scroll', handleScroll)
-    return () => window.removeEventListener('scroll', handleScroll)
-  }, [])
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <Cursor />
-        <Navigation />
-        <Gallery />
-        
+        hi
       </div>
     </main>
   )
